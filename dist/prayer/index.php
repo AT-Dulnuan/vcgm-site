b5a1e784428a5640df2ae42725fbf90b5e1dfa59
Abbi <at.dulnuan@gmail.com>
2020-06-15 22:18:05 +0800

    Add under construction page.

--- dist/prayer/index.php
+++ dist/prayer/index.php
@@ -66,6 +66,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VCGM | Prayer Request</title>
 
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="../assets/favicon/site.webmanifest">
+
     <!-- Styles -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
