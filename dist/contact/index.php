590a443e1d173f2bd7c56a4cd4d2fe4a9c51b53a
Abbi <at.dulnuan@gmail.com>
2020-08-29 10:07:38 +0400

    Fix contact page and locations page. Add core values. Soft release.

--- dist/contact/index.php
+++ dist/contact/index.php
@@ -89,11 +89,8 @@
                             <li><a href="../about">ABOUT</a></li>
                             <li><a href="../locations">LOCATIONS</a></li>
                             <li><a href="../sermons">SERMONS</a></li>
+                            <li><a class="is-active" href="../contact">CONTACT US</a></li>
                         </ul>
-                        <div class="btn-container">
-                            <a href="./" class="btn btn-outline hover-btn" id="connect-btn">Lets
-                                Connect</a>
-                        </div>
                     </div>
                     <div id="menuBurger" onclick="openMenu()">
                         <span></span>
@@ -103,88 +100,87 @@
                     </div>
                 </div>
             </nav>
-
-            <div class="header-text">
-                <h1 data-aos="fade-down" data-aos-duration="1000">Contact Us</h1>
-            </div>
         </header>
 
-        <section class="contact-form">
-            <div class="container">
-                <?php if($msgText != ''): ?>
-                <div class="notification <?php echo $msgType; ?> is-light has-text-centered">
-                    <?php echo $msgText; ?>
-                    <?php if($msgType == 'is-success') : ?>
-                    <div class="container has-text-centered">
-                        <a href="../" class="button is-text return-btn">Return to Home</a>
+        <div class="wrapper">
+            <div class="columns">
+                <section class="column is-two-thirds contact-form">
+                    <div class="container">
+                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" novalidate>
+                            <h3 class=" title is-2 has-text-white">Reach out to us!</h3>
+                            <p class="subtitle is-6 has-text-white">Fill out the form and our team will get back to you the
+                                soonest.</p>
+                            <?php if($msgText != ''): ?>
+                            <div class="notification <?php echo $msgType; ?> is-light has-text-centered">
+                                <?php echo $msgText; ?>
+                            </div>
+                            <?php endif; ?>
+                            <div class="field">
+                                <label class="label">Full Name</label>
+                                <div class="control has-icons-left">
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-user"></i>
+                                    </span>
+                                    <input class="input" type="text" name="fullName" placeholder="John Doe"
+                                        value="<?php echo isset($_POST['fullName']) ? $fullName : ''; ?>">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">Email</label>
+                                <div class="control has-icons-left">
+                                    <input class="input <?php echo $emailError; ?>" type="email" name="email"
+                                        placeholder="john.doe@mail.com"
+                                        value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-envelope"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">Message</label>
+                                <div class="control">
+                                    <textarea class="textarea" name="message"
+                                        placeholder="Type your message here..."><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
+                                </div>
+                            </div>
+                            <button class="btn hover-btn button send has-text-primary has-text-weight-bold" name="submit"
+                                type="submit" <?php echo $disableForm; ?>>Submit</button>
+                        </form>
                     </div>
-                    <?php endif;?>
-                </div>
-                <?php endif; ?>
-                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" novalidate>
-                    <div class="field">
-                        <label class="label">Full Name</label>
-                        <div class="control has-icons-left">
-                            <span class="icon is-small is-left">
-                                <i class="fas fa-user"></i>
-                            </span>
-                            <input class="input" type="text" name="fullName" placeholder="John Doe"
-                                value="<?php echo isset($_POST['fullName']) ? $fullName : ''; ?>">
-                        </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">Email</label>
-                        <div class="control has-icons-left">
-                            <input class="input <?php echo $emailError; ?>" type="email" name="email"
-                                placeholder="john.doe@mail.com"
-                                value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
-                            <span class="icon is-small is-left">
-                                <i class="fas fa-envelope"></i>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">Message</label>
-                        <div class="control">
-                            <textarea class="textarea" name="message"
-                                placeholder="Type your message here..."><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
-                        </div>
-                    </div>
-                    <button class="button send has-text-white" name="submit" type="submit"
-                        <?php echo $disableForm; ?>>Submit</button>
-                </form>
-            </div>
-        </section>
+                </section>
 
-        <footer>
-            <div class="row">
-                <div class="brand">
-                    <img src="" alt="">
-                    <p>We are a Pentecostal movement whose heart is in building up transformed and empowered communities
-                        of faith with a desire to live out God’s Word and reflect His service and ministry so we can
-                        influence the world.</p>
-                </div>
-                <div class="links">
-                    <ul>
-                        <li><a href="../">HOME</a></li>
-                        <li><a href="../about">ABOUT</a></li>
-                        <li><a href="../locations">LOCATIONS</a></li>
-                        <li><a href="../sermons">SERMONS</a></li>
-                    </ul>
-                </div>
-                <div class="connect">
-                    <p>Lets Connect!</p>
-                    <ul>
-                    <a href="https://www.instagram.com/vcgmchurch/" target="_blank"><i
-                                class="fab fa-instagram"></i></a>
-                        <a href="https://www.youtube.com/channel/UCdEjO9AOccUIS_IMNirLtMg/videos" target="_blank"><i
-                                class="fab fa-youtube"></i></a>
-                        <a href="https://www.facebook.com/vcgmofficial/" target="_blank"><i
-                                class="fab fa-facebook"></i></a>
-                    </ul>
-                </div>
+                <section class="column email-adds">
+                    <div class="container has-text-white">
+                        <h2 class="title is-2 has-text-white mb-1">Contact Information</h2>
+                        <ul>
+                            <li><a class="emails" target="_blank"
+                                    href="mailto:&#97;&#100;&#109;&#105;&#110;&#64;&#118;&#99;&#103;&#109;&#99;&#104;&#117;&#114;&#99;&#104;&#46;&#111;&#114;&#103;"><i
+                                        class="fas fa-question-circle"></i> Admin</a>
+                            </li>
+                            <li><a class="emails" target="_blank"
+                                    href="mailto:&#118;&#105;&#115;&#105;&#111;&#110;&#99;&#111;&#108;&#108;&#101;&#103;&#101;&#64;&#118;&#99;&#103;&#109;&#99;&#104;&#117;&#114;&#99;&#104;&#46;&#111;&#114;&#103;"><i
+                                        class="fas fa-university"></i> Vision
+                                    Leadership College</a>
+                            </li>
+                        </ul>
+
+                        <h2 class="title is-2 has-text-white follow mb-1">Follow Us</h2>
+                        <ul class="follow-socials">
+                            <li><a href="https://www.instagram.com/vcgmchurch/" target="_blank"><i
+                                        class="fab fa-instagram"></i></a>
+                            </li>
+                            <li><a href="https://www.youtube.com/channel/UCdEjO9AOccUIS_IMNirLtMg/videos" target="_blank"><i
+                                        class="fab fa-youtube"></i></a>
+                            </li>
+                            <li><a href="https://www.facebook.com/vcgmofficial/" target="_blank"><i
+                                        class="fab fa-facebook"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
             </div>
-        </footer>
+        </div>
+
     </div>
 
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
